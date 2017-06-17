aae71e43880bf505c73364a9209a31fba2ea8741
Jaakko Lipsanen <jaakko.lipsanen@outlook.com>
2017-06-18 00:03:35 +0300

    g

--- src/commands/route-command/index.js
+++ src/commands/route-command/index.js
@@ -1,4 +1,4 @@
-const Command = require('../command');
+const { Command } = require('tg-commands');
 const helpers = require('../../helpers');
 
 const chartRenderer = require('./chart-renderer');
@@ -13,19 +13,19 @@ class RouteCommand extends Command {
 		super(...args);
 	}
 
-	async run({ context, parameters }) {
-		const waypoints = await this.getWaypoints(context, parameters);
+	async run(ctx, params) {
+		const waypoints = await this.getWaypoints(ctx, params);
 
 		const routeResult = await this.getRouteFrom(waypoints);
 		if(!routeResult.success) {
-			context.sendText(routeResult.error.message);
+			ctx.sendText(routeResult.error.message);
 			return;
 		}
 
 		const route = routeResult.payload.route;
 		const chart = await chartRenderer.renderChart(route);
 
-		context.sendPhoto(chart);
+		ctx.sendPhoto(chart);
 	}
 
 	async getRouteFrom(waypoints) {
--- src/server.js
+++ src/server.js
@@ -1,12 +1,54 @@
 require('dotenv').config();
 
-const TelegramBot = require('tg-commands');
+const { TelegramBot } = require('tg-commands');
 const RouteCommand = require('./commands/route-command');
 
 const token = process.env.TG_BOT_TOKEN;
 const bot = new TelegramBot({ apiToken: token });
 
 bot.register('route', RouteCommand);
+bot.register('test', async (ctx, params) => {
+	ctx.sendText("Tell me tales");
+	const msg = await ctx.waitForResponse();
+	if(msg.text) {
+		ctx.sendText("That tale sucked");
+	}
+	else {
+		ctx.sendText("That wasn't a tale :heart:");
+	}
+});
+
+bot.register('hala', {
+	async run(ctx, params) {
+		ctx.sendText("Tell me tales");
+		const msg = await ctx.askForText("What's your name?");
+		if(msg.text) {
+			ctx.sendText("That tale sucked");
+		}
+		else {
+			ctx.sendText("That wasn't a tale :heart:");
+		}
+	},
+
+	abort(ctx) {
+
+	}
+});
+
+await ctx.waitForMessage({
+	retryLimit: 1,
+	accept: (ctx, msg) => {
+		if(msg.text) return true;
+
+		ctx.sendText("Please send a text message!");
+	}
+});
+
+await ctx.helpers.waitForText({
+	retryLimit: 1,
+	wrongTypeMessage: "Please send a text message"
+});
+
 bot.start();
 
 // show more detailed errors for unhandled promise exceptions
